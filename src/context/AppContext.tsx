765d51b7b6c52a3b0b0c543cd488d5d8e90f81ea
YDan08 <dasyamashita@gmail.com>
2023-06-29 15:16:14 -0300

    refactor: alterar useState para useReducer

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,17 @@
+import { createContext, useReducer, useState, useContext } from "react"
 import { ProdutosCarrinho } from "@/types/context"
 import { Produto } from "@/types/produto"
-import { createContext, useState } from "react"
+
+type State = {
+	carrinho: ProdutosCarrinho[]
+}
+
+type Action =
+	| { type: "add"; produto: Produto; quantidade: number }
+	| { type: "remove"; codigo: number }
+	| { type: "increment"; codigo: number }
+	| { type: "decrement"; codigo: number }
+	| { type: "reset" }
 
 interface AppContextProps {
 	carrinho?: ProdutosCarrinho[]
@@ -20,75 +31,126 @@ export const AppContext = createContext<AppContextProps>({
 	handleReset: () => {},
 })
 
-export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-	const [carrinho, setCarrinho] = useState<ProdutosCarrinho[]>([])
-	const handleAdd = (produto: Produto, quantidade: number) => {
-		const existsProduto = carrinho.some(
-			item => item.produto.codigo_produto === produto.codigo_produto
-		)
+export const useCarrinho = () => useContext(AppContext)
 
-		if (!existsProduto) {
-			return setCarrinho(prevProdutos => [...prevProdutos, { produto, quantidade }])
+function reducer(state: State, action: Action) {
+	switch (action.type) {
+		case "add": {
+			const existsProduto = state.carrinho.some(
+				item => item.produto.codigo_produto === action.produto.codigo_produto
+			)
+
+			if (!existsProduto) {
+				return {
+					...state,
+					carrinho: [
+						...state.carrinho,
+						{ produto: action.produto, quantidade: action.quantidade },
+					],
+				}
+			}
+
+			return {
+				...state,
+				carrinho: state.carrinho.map(prevProduto =>
+					prevProduto.produto.codigo_produto === action.produto.codigo_produto
+						? { ...prevProduto, quantidade: prevProduto.quantidade + action.quantidade }
+						: prevProduto
+				),
+			}
 		}
 
-		setCarrinho(prevProdutos =>
-			prevProdutos.map(prevProduto =>
-				prevProduto.produto.codigo_produto === produto.codigo_produto
-					? { ...prevProduto, quantidade: prevProduto.quantidade + quantidade }
-					: prevProduto
+		case "remove": {
+			return {
+				...state,
+				carrinho: state.carrinho.filter(
+					prevProduto => prevProduto.produto.codigo_produto != action.codigo
+				),
+			}
+		}
+
+		case "increment": {
+			const existsProduto = state.carrinho.some(
+				produto => produto.produto.codigo_produto === action.codigo
 			)
-		)
+
+			if (existsProduto) {
+				return {
+					...state,
+					carrinho: state.carrinho.map(prevProduto =>
+						prevProduto.produto.codigo_produto === action.codigo
+							? { ...prevProduto, quantidade: prevProduto.quantidade + 1 }
+							: prevProduto
+					),
+				}
+			}
+		}
+
+		case "decrement": {
+			const produto = state.carrinho.find(
+				item => item.produto.codigo_produto === action.codigo
+			)
+
+			if (!produto) return state
+
+			if (produto.quantidade <= 1) {
+				return {
+					...state,
+					carrinho: state.carrinho.filter(
+						preProduto => preProduto.produto.codigo_produto !== action.codigo
+					),
+				}
+			}
+
+			return {
+				...state,
+				carrinho: state.carrinho.map(preProduto =>
+					preProduto.produto.codigo_produto === action.codigo
+						? { ...preProduto, quantidade: preProduto.quantidade - 1 }
+						: preProduto
+				),
+			}
+		}
+
+		case "reset": {
+			return {
+				...state,
+				carrinho: [],
+			}
+		}
+
+		default:
+			return state
+	}
+}
+
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+	const [state, dispatch] = useReducer(reducer, { carrinho: [] })
+
+	const handleAdd = (produto: Produto, quantidade: number) => {
+		dispatch({ type: "add", produto, quantidade })
 	}
 
 	const handleRemove = (codigo: number) => {
-		setCarrinho(prevProdutos =>
-			prevProdutos.filter(produto => produto.produto.codigo_produto != codigo)
-		)
+		dispatch({ type: "remove", codigo })
 	}
 
 	const handleReset = () => {
-		setCarrinho([])
+		dispatch({ type: "reset" })
 	}
 
 	const handleProductIncrement = (codigo: number) => {
-		const existsProduto = carrinho.some(
-			produto => produto.produto.codigo_produto === codigo
-		)
-
-		if (existsProduto) {
-			setCarrinho(prevProdutos =>
-				prevProdutos.map(prevProduto =>
-					prevProduto.produto.codigo_produto === codigo
-						? { ...prevProduto, quantidade: prevProduto.quantidade + 1 }
-						: prevProduto
-				)
-			)
-		}
+		dispatch({ type: "increment", codigo })
 	}
 
 	const handleProductDecrement = (codigo: number) => {
-		const produto = carrinho.find(item => item.produto.codigo_produto === codigo)
-		if (produto) {
-			if (produto.quantidade <= 1) {
-				setCarrinho(prevProduto =>
-					prevProduto.filter(prevProduto => prevProduto.produto.codigo_produto !== codigo)
-				)
-			} else {
-				setCarrinho(prevProduto =>
-					prevProduto.map(prevProduto =>
-						prevProduto.produto.codigo_produto === codigo
-							? { ...prevProduto, quantidade: prevProduto.quantidade - 1 }
-							: prevProduto
-					)
-				)
-			}
-		}
+		dispatch({ type: "decrement", codigo })
 	}
 
 	return (
 		<AppContext.Provider
 			value={{
-				carrinho,
+				carrinho: state.carrinho,
 				handleAdd,
 				handleRemove,
 				handleProductDecrement,
