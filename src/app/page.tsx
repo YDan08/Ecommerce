900b181f742fd23ef6e5658a79b97947c88d786a
YDan08 <dasyamashita@gmail.com>
2023-06-29 13:57:58 -0300

    feat: criar componente produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ProdutoItem from "@/components/ProdutoItem"
 import { ListaProduto } from "@/types/produto"
 import axios from "axios"
 import Image from "next/image"
@@ -12,18 +13,7 @@ const Home = async () => {
 		<main className='flex-1 p-12'>
 			<div className='grid gap-8 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
 				{produtos.map(produto => (
-					<div
-						className='flex flex-col items-center rounded-2xl bg-white p-6'
-						key={produto.codigo_produto}
-					>
-						<Image src={produto.imagem_produto} alt='foto' width={80} height={80} />
-						<h1 className='my-3 text-lg font-semibold'>{produto.nome_produto}</h1>
-						<Link href={`products/${produto.codigo_produto}`}>
-							<button className='bg-green-600 py-2 px-7 rounded-lg text-white text-sm'>
-								Comprar
-							</button>
-						</Link>
-					</div>
+					<ProdutoItem key={produto.codigo_produto} produto={produto} />
 				))}
 			</div>
 		</main>
---
+++ src/components/ProdutoItem.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useContext } from "react"
+import { AppContext } from "@/context/AppContext"
+import { ListaProduto, Produto } from "@/types/produto"
+import Image from "next/image"
+import Link from "next/link"
+
+interface ProdutoItemProps {
+	produto: ListaProduto
+}
+
+const transformProduto = (produto: ListaProduto): Produto => {
+	return {
+		...produto,
+		nome: produto.nome_produto,
+		codigo_produto: Number(produto.codigo_produto),
+	} as unknown as Produto
+}
+
+export const ProdutoItem = ({ produto }: ProdutoItemProps) => {
+	const { handleAdd } = useContext(AppContext)
+
+	const handleBuy = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault()
+		e.stopPropagation()
+		handleAdd(transformProduto(produto), 1)
+	}
+
+	return (
+		<Link href={`products/${produto.codigo_produto}`} key={produto.codigo_produto}>
+			<div className='flex flex-col items-center rounded-2xl bg-white p-6'>
+				<Image src={produto.imagem_produto} alt='foto' width={80} height={80} />
+				<h1 className='my-3 text-lg font-semibold'>{produto.nome_produto}</h1>
+				<button
+					className='bg-green-600 py-2 px-7 rounded-lg text-white text-sm'
+					onClick={handleBuy}
+				>
+					Adicionar ao carrinho
+				</button>
+			</div>
+		</Link>
+	)
+}
+
+export default ProdutoItem
