149d2458dcad30fbe3f9a87d8cc2cb27054945ec
YDan08 <dasyamashita@gmail.com>
2023-06-29 13:57:40 -0300

    feat: adicionar link ao botaoAdicionar

--- src/components/BotaoAdicionar.tsx
+++ src/components/BotaoAdicionar.tsx
@@ -2,6 +2,8 @@
 import { AppContext } from "@/context/AppContext"
 import { ProductContext } from "@/context/ProductContext"
 import { Produto } from "@/types/produto"
+import Link from "next/link"
+
 import { useContext } from "react"
 
 interface BotaoAdicionarProps {
@@ -12,11 +14,12 @@ export const BotaoAdicionar = ({ produto }: BotaoAdicionarProps) => {
 	const { quantidade } = useContext(ProductContext)
 	const { handleAdd } = useContext(AppContext)
 	return (
-		<button
+		<Link
+			href={"/carrinho"}
 			className='flex-1 py-3 text-center bg-green-600 rounded-lg text-white'
 			onClick={() => handleAdd(produto, quantidade)}
 		>
 			Adicionar ao carrinho
-		</button>
+		</Link>
 	)
 }
