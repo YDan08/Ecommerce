26702ad38054342a922c9f7a8e73e23a9d5ac12a
YDan08 <dasyamashita@gmail.com>
2023-06-29 18:25:48 -0300

    feat: criar capitalize

---
+++ src/utils/string.util.ts
@@ -0,0 +1,8 @@
+export function capitalize(str: string): string {
+  return str
+    .split(' ')
+    .map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    })
+    .join(' ')
+}
