ec6d0daadb13c3f5c885937c961ff6fd13a6e04e
YDan08 <dasyamashita@gmail.com>
2023-06-29 19:12:45 -0300

    feat: adicionar link github

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import { Github } from 'lucide-react'
+import Link from 'next/link'
+
 export const Footer = () => {
   return (
-    <footer className="flex justify-center bg-slate-100 py-4 text-gray-700">
+    <footer className="flex items-center justify-around bg-slate-100 py-4 text-gray-700">
       <p>Desenvolvido por Daniel Yamashita</p>
+      <Link href="https://github.com/YDan08/Ecommerce" className="flex gap-1" target="_blank">
+        <Github />
+        <span className="font-semibold">Repositório</span>
+      </Link>
     </footer>
   )
 }
