754359ffd4d1adc148279e4ed97e0f263ee2a753
YDan08 <dasyamashita@gmail.com>
2023-06-29 04:41:30 -0300

    feat: adicionar pacotes e adicionar configurações

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ["innovationbrindes.com.br"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "innovationbrindes.com.br",
+				port: "",
+				pathname: "/images/**",
+			},
+		],
+	},
+}
 
 module.exports = nextConfig
