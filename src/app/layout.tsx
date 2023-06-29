3bd30ed3dd87707e03db3c407c78848262f57fb2
YDan08 <dasyamashita@gmail.com>
2023-06-29 18:29:54 -0300

    feat: adicionar navbar e favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
 'use client'
 
 import { Inter } from 'next/font/google'
+import { NavBar } from '@/components/NavBar'
 import { Footer } from '@/components/Footer'
 import { AppProvider } from '../context/AppContext'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Ecommerce',
+  description: 'Projeto de ecommerce'
+}
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={`text-slate-950 ${inter.className}`}>
         <div className="flex h-screen flex-col">
-          <AppProvider>{children}</AppProvider>
-          <Footer />
+          <AppProvider>
+            <NavBar />
+            {children}
+            <Footer />
+          </AppProvider>
         </div>
       </body>
     </html>
