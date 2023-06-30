b4917a91323f039567544c87b7205c6b7a6f359f
YDan08 <dasyamashita@gmail.com>
2023-06-30 01:50:44 -0300

    refactor: validar array e baseurl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,17 @@ const Home = async () => {
 
   return (
     <main className="flex-1 px-5 py-10 sm:p-12">
-      <div className="mx-auto grid max-w-7xl gap-12 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        {produtos.map(produto => (
-          <ProdutoItem key={produto.codigo_produto} produto={produto} />
-        ))}
-      </div>
+      {produtos.length !== 0 ? (
+        <div className="mx-auto grid max-w-7xl gap-12 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+          {produtos.map(produto => (
+            <ProdutoItem key={produto.codigo_produto} produto={produto} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center">
+          <p className="text-center font-semibold">Não existem produtos disponiveis!</p>
+        </div>
+      )}
     </main>
   )
 }
--- src/models/product.server.ts
+++ src/models/product.server.ts
@@ -1,19 +1,20 @@
 import axios from 'axios'
 import { Produto, ListaProduto } from '@/types/produto'
 
-export const getProdutos = async (): Promise<ListaProduto[]> => {
-  const response = await axios.get<ListaProduto[]>(
-    'https://apihomolog.innovationbrindes.com.br/api/site/v2/teste/listagem-produtos'
-  )
+const baseUrl = 'https://apihomolog.innovationbrindes.com.br/api/site/v2'
 
-  return response.data
+export const getProdutos = async (): Promise<ListaProduto[]> => {
+  try {
+    const response = await axios.get<ListaProduto[]>(`${baseUrl}/teste/listagem-produtos`)
+    return response.data
+  } catch {
+    return []
+  }
 }
 
 export const getProdutoById = async (id: string): Promise<Produto | null> => {
   try {
-    const response = await axios.get<Produto>(
-      `https://apihomolog.innovationbrindes.com.br/api/site/v2/produto/${id}`
-    )
+    const response = await axios.get<Produto>(`${baseUrl}/produto/${id}`)
 
     return response.data
   } catch {
