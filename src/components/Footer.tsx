57ff141058333882813fd109ad03a2171fd4cde3
YDan08 <dasyamashita@gmail.com>
2023-06-29 23:03:05 -0300

    feat: adicionar blank no link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ export const Footer = () => {
     <footer className="flex flex-col items-center justify-around gap-4 bg-slate-100 py-4 text-gray-700 sm:flex-row sm:justify-center">
       <p className="text-sm sm:text-base">Desenvolvido por Daniel Yamashita</p>
       <div className="flex gap-x-4">
-        <Link href="https://github.com/YDan08/Ecommerce" className="flex gap-1" target="_blank">
+        <Link href="https://github.com/YDan08/Ecommerce" target="_blank">
           <Github />
         </Link>
-        <Link href="https://www.linkedin.com/in/daniel-yamashita-35b742208/">
+        <Link href="https://www.linkedin.com/in/daniel-yamashita-35b742208/" target="_blank">
           <Linkedin />
         </Link>
       </div>
